791fb14255e6f97ab1f0cd132aa6274f31b2b733
venom-kitty <168876244+venom-kitty@users.noreply.github.com>
2024-06-05 02:16:04 -0400

    miniDDoS Batch file
    the file that all of the code runs on.

---
+++ miniDDoS.bat
@@ -0,0 +1,17 @@
+@echo off
+
+title miniDDoS
+
+echo       ____  ____  _____  ___ 
+echo      (  _ \(  _ \(  _  )/ __) !
+echo       )(_) ))(_) ))(_)( \__ \ !
+echo mini (____/(____/(_____)(___/ !
+echo made in batch by ven !!
+echo =================================
+
+set /p input=Target IP: 
+
+title miniDDos started, do not close this window!
+echo.
+echo miniDDoS attack started.
+ping %input% -t
